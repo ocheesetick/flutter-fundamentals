5757f6042caa0937a8ebe7c80e5e94448f146b41
JT Rael <raeljohntimothy@gmail.com>
2024-04-15 17:20:10 +0800

    Bottom navigation bar

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,47 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:fundamentals/pages/home_page.dart';
+import 'package:fundamentals/pages/profile_page.dart';
+import 'package:fundamentals/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget {
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [HomePage(), ProfilePage(), SettingsPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("1st Page")),
-      drawer: Drawer(
-          backgroundColor: Colors.deepPurple[100],
-          child: Column(
-            children: [
-              DrawerHeader(
-                  child: Icon(
-                Icons.favorite,
-                size: 48,
-              )),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          // home
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
 
-              // Home page list tile
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
-                onTap: () {
-                  // pop/close drawer first
-                  Navigator.pop(context);
-                  // then go to the route
-                  Navigator.pushNamed(context, '/homepage');
-                },
-              ),
+          // profile
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
 
-              // Home page list tile
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text("Settings"),
-                onTap: () {
-                  // pop/close drawer first
-                  Navigator.pop(context);
-                  // then go to the route
-                  Navigator.pushNamed(context, '/settingspage');
-                },
-              )
-            ],
-          )),
+          // settings
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+        ],
+      ),
     );
   }
 }
